f17517344c8f42c96cbcc33135d8f87c47b7c8a3
a.dolzhonok <A.Dolzhonok@m-developer.ru>
2024-04-04 23:20:33 +0300

    Lesson_12_junit5

--- src/test/java/org/example/FactorialTest.java
+++ src/test/java/org/example/FactorialTest.java
@@ -1,12 +1,10 @@
 package org.example;
 
-
 import static org.junit.Assert.assertTrue;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
 
 public class FactorialTest {
 
